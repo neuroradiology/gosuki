1c0cf2db055aff5c785aa5dfaff7548472e3fa0a
Chakib Ben Ziane <contact@blob42.xyz>
2017-11-16 14:27:50 +0100

    better code architecture for bookmarks,db, and watcher

--- db.go
+++ db.go
@@ -9,14 +9,13 @@ import (
 )
 
 const (
-	DB_LOCAL    = "bookmarks.db"
-	DB_MEMCACHE = "file:memcache?mode=memory&cache=shared"
-	DB_CURRENT  = "file:currentjob?mode=memory&cache=shared&_busy_timeout=5000000"
+	DB_LOCAL_PATH    = "bookmarks.db"
+	DB_MEMCACHE_PATH = "file:memcache?mode=memory&cache=shared"
+	DB_BUFFER_PATH   = "file:buffer?mode=memory&cache=shared"
 )
 
 var (
-	memCacheDb   *sql.DB
-	currentJobDB *sql.DB
+	CACHE_DB *DB // Main in memory db, is synced with disc
 )
 
 const (
@@ -39,6 +38,204 @@ const (
 		flags integer default 0
 	)`
 )
+
+type DB struct {
+	name   string
+	path   string
+	handle *sql.DB
+}
+
+func (db *DB) Init() error {
+
+	var err error
+
+	// Create the memory cache db
+	db.handle, err = sql.Open("sqlite3", db.path)
+	debugPrint("db <%s> opend at at <%s>", db.name, db.path)
+	if err != nil {
+		return err
+	}
+
+	// Populate db schema
+	_, err = db.handle.Exec(CREATE_MEM_DB_SCHEMA)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *DB) Close() {
+	db.handle.Close()
+}
+
+func (db *DB) Count() int {
+	var count int
+
+	row := db.handle.QueryRow("select count(*) from bookmarks")
+	err := row.Scan(&count)
+	logPanic(err)
+
+	return count
+}
+
+func (src *DB) SyncTo(dst *DB) error {
+
+	debugPrint("Syncing <%s>(%d) to <%s>(%d)", src.name,
+		src.Count(),
+		dst.name,
+		dst.Count())
+
+	conns := []*sqlite3.SQLiteConn{}
+
+	sql.Register("sqlite_with_backup",
+		&sqlite3.SQLiteDriver{
+			ConnectHook: func(conn *sqlite3.SQLiteConn) error {
+				conns = append(conns, conn)
+
+				return nil
+			},
+		})
+
+	srcDb, err := sql.Open("sqlite_with_backup", src.path)
+	defer srcDb.Close()
+	if err != nil {
+		return err
+	}
+
+	srcDb.Ping()
+
+	dstDb, err := sql.Open("sqlite_with_backup", dst.path)
+	defer dstDb.Close()
+	if err != nil {
+		return err
+	}
+	dstDb.Ping()
+
+	bk, err := conns[1].Backup("main", conns[0], "main")
+	if err != nil {
+		return err
+	}
+
+	_, err = bk.Step(-1)
+	if err != nil {
+		return err
+	}
+
+	bk.Finish()
+
+	return nil
+}
+
+func (db *DB) FlushToDisk() error {
+
+	conns := []*sqlite3.SQLiteConn{}
+
+	sql.Register("sqlite_with_backup",
+		&sqlite3.SQLiteDriver{
+			ConnectHook: func(conn *sqlite3.SQLiteConn) error {
+				conns = append(conns, conn)
+
+				return nil
+			},
+		})
+
+	srcDb, err := sql.Open("sqlite_with_backup", db.path)
+	if err != nil {
+		return err
+	}
+	defer srcDb.Close()
+
+	srcDb.Ping()
+
+	bkDb, err := sql.Open("sqlite_with_backup", DB_LOCAL_PATH)
+	defer bkDb.Close()
+	if err != nil {
+		return err
+	}
+	bkDb.Ping()
+
+	bk, err := conns[1].Backup("main", conns[0], "main")
+	if err != nil {
+		return err
+	}
+
+	_, err = bk.Step(-1)
+	if err != nil {
+		return err
+	}
+
+	bk.Finish()
+
+	return nil
+}
+
+// TODO: Use context when making call from request/api
+func initDB() error {
+	debugPrint("[NotImplemented] initialize local db if not exists or load it")
+	// Check if db exists locally
+
+	// If does not exit create new one in memory
+	// Then flush to disk
+
+	// If exists locally, load to memory
+
+	// Initialize memory db with schema
+	CACHE_DB = &DB{"memcache",
+		DB_MEMCACHE_PATH, nil}
+
+	err := CACHE_DB.Init()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// TODO: Use context when making call from request/api
+//func initMemCacheDb() {
+// Init both cache and current memory dbs
+// `Cache` is a memory replica of disk db
+// `Current` is current working job db
+//
+//sqlite3conn := []*sqlite3.SQLiteConn{}
+
+//var err error
+//memCacheDb := &DB{"memcache",
+//DB_MEMCACHE}
+
+//// Create the memory cache db
+//memCacheDb.handle, err = sql.Open("sqlite3", memCacheDb.path)
+//debugPrint("in memory memCacheDb opened at %s", memCacheDb.path)
+//logPanic(err)
+
+//_, err = memCacheDb.Exec(CREATE_MEM_DB_SCHEMA)
+//logPanic(err)
+
+// Create the current job db
+//currentJobDB, err = sql.Open("sqlite3", DB_CURRENT)
+//debugPrint("in memory currentJobDb opened")
+//logPanic(err)
+
+//_, err = currentJobDB.Exec(CREATE_MEM_DB_SCHEMA)
+//logPanic(err)
+
+//}
+
+func testInMemoryDb(db *DB) {
+
+	debugPrint("test in memory")
+	_db, err := sql.Open("sqlite3", db.path)
+	defer _db.Close()
+	rows, err := _db.Query("select URL from bookmarks")
+	defer rows.Close()
+	logPanic(err)
+	var URL string
+	for rows.Next() {
+		rows.Scan(&URL)
+		log.Println(URL)
+	}
+}
 
 func isEmptyDb(db *sql.DB) bool {
 	var count int
@@ -78,59 +275,6 @@ func printDB(db *sql.DB) {
 
 }
 
-// TODO: Use context when making call from request/api
-func initInMemoryDb() {
-	// Init both cache and current memory dbs
-	// `Cache` is a memory replica of disk db
-	// `Current` is current working job db
-	//
-	//sqlite3conn := []*sqlite3.SQLiteConn{}
-
-	var err error
-
-	// Create the memory cache db
-	memCacheDb, err = sql.Open("sqlite3", DB_MEMCACHE)
-	debugPrint("in memory memCacheDb opened")
-	logPanic(err)
-
-	// Create the current job db
-	currentJobDB, err = sql.Open("sqlite3", DB_CURRENT)
-	debugPrint("in memory currentJobDb opened")
-	logPanic(err)
-
-	_, err = memCacheDb.Exec(CREATE_MEM_DB_SCHEMA)
-	logPanic(err)
-	_, err = currentJobDB.Exec(CREATE_MEM_DB_SCHEMA)
-	logPanic(err)
-
-}
-
-func initDB() {
-	debugPrint("[NotImplemented] initialize local db if not exists or load it")
-	// Check if db exists locally
-
-	// If does not exit create new one in memory
-	// Then flush to disk
-
-	// If exists locally, load to memory
-	initInMemoryDb()
-}
-
-func testInMemoryDb() {
-
-	debugPrint("test in memory")
-	db, err := sql.Open("sqlite3", DB_MEMCACHE)
-	defer db.Close()
-	rows, err := db.Query("select URL from bookmarks")
-	defer rows.Close()
-	logPanic(err)
-	var URL string
-	for rows.Next() {
-		rows.Scan(&URL)
-		log.Println(URL)
-	}
-}
-
 func syncToDB(src string, dst string) {
 
 	conns := []*sqlite3.SQLiteConn{}
@@ -162,46 +306,4 @@ func syncToDB(src string, dst string) {
 	logPanic(err)
 
 	bk.Finish()
-}
-
-func flushToDisk() {
-	/// Flush in memory sqlite db to disk
-	/// should happen as often as possible to
-	/// avoid losing data
-
-	// TODO
-	// Memory db and disk db should not be the same
-	// Memory db is used to load all urls even duplicates
-	debugPrint("Flushing to disk")
-
-	conns := []*sqlite3.SQLiteConn{}
-
-	sql.Register("sqlite_with_backup",
-		&sqlite3.SQLiteDriver{
-			ConnectHook: func(conn *sqlite3.SQLiteConn) error {
-				conns = append(conns, conn)
-
-				return nil
-			},
-		})
-
-	memCacheDb, err := sql.Open("sqlite_with_backup", DB_MEMCACHE)
-	logPanic(err)
-	defer memCacheDb.Close()
-
-	memCacheDb.Ping()
-
-	bkDb, err := sql.Open("sqlite_with_backup", DB_LOCAL)
-	defer bkDb.Close()
-	logPanic(err)
-	bkDb.Ping()
-
-	bk, err := conns[1].Backup("main", conns[0], "main")
-	logPanic(err)
-
-	_, err = bk.Step(-1)
-	logPanic(err)
-
-	bk.Finish()
-
 }
--- watcher.go
+++ watcher.go
@@ -1,26 +1,89 @@
 package main
 
 import (
+	"database/sql"
 	"log"
+	"path"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
 
-func watcherThread(watcher *fsnotify.Watcher) {
+type MarkTypes int
+
+const (
+	Chrome MarkTypes = iota
+	Firefox
+)
+
+type bookmarkWatcher struct {
+	watcher   *fsnotify.Watcher
+	baseDir   string
+	bkFile    string
+	parseFunc func(*bookmarkWatcher)
+	bufferDB  *sql.DB
+}
+
+func (bw *bookmarkWatcher) Close() error {
+	if err := bw.watcher.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (bw *bookmarkWatcher) Init(basedir string, bkfile string, bkType MarkTypes) *bookmarkWatcher {
+	var err error
+
+	bw.baseDir = basedir
+	bw.bkFile = bkfile
+
+	bw.watcher, err = fsnotify.NewWatcher()
+	logPanic(err)
+
+	switch bkType {
+	case Chrome:
+		bw.parseFunc = googleParseBookmarks
+	}
+
+	return bw
+}
+
+func (bw *bookmarkWatcher) Start() error {
+
+	if err := bw.watcher.Add(bw.baseDir); err != nil {
+		return err
+	}
+
+	go bWatcherThread(bw, bw.parseFunc)
+
+	return nil
+}
+
+func bWatcherThread(bw *bookmarkWatcher, parseFunc func(bw *bookmarkWatcher)) {
+
+	bookmarkPath := path.Join(bw.baseDir, bw.bkFile)
+	debugPrint("watching %s", bookmarkPath)
+
 	for {
 		select {
-		case event := <-watcher.Events:
+		case event := <-bw.watcher.Events:
 
 			if event.Op&fsnotify.Create == fsnotify.Create &&
-				event.Name == BOOKMARK_FILE {
+				event.Name == bookmarkPath {
 
 				debugPrint("event: %v| name: %v", event.Op, event.Name)
 				debugPrint("modified file:", event.Name)
-				googleParseBookmarks(BOOKMARK_FILE)
+				start := time.Now()
+				parseFunc(bw)
+				elapsed := time.Since(start)
+				debugPrint("parsing in %s", elapsed)
 
 			}
-		case err := <-watcher.Errors:
+		case err := <-bw.watcher.Errors:
 			log.Println("error:", err)
 		}
 	}
+
+	debugPrint("Exiting watch thread")
 }
