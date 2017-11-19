e1ab49d9862ca4b5dc0f1bfa8feeba80b5c92f31
Chakib Ben Ziane <contact@blob42.xyz>
2017-11-19 21:28:24 +0100

    wip browser interface

--- browsers.go
+++ browsers.go
@@ -1,8 +1,35 @@
 package main
 
+import (
+	"database/sql"
+
+	fsnotify "gopkg.in/fsnotify.v1"
+)
+
 type BrowserType uint8
 
 const (
-	Chrome BrowserType = iota
-	Firefox
+	TChromeBrowser BrowserType = iota
+	FirefoxBrowser
 )
+
+type Browser interface {
+	New(BrowserType) *Browser // Creates and initialize new browser
+	Watch() *fsnotify.Watcher // Starts watching bookmarks and runs Load on change
+	Load()                    // Loads bookmarks to db without watching
+	Parse()                   // Main parsing method
+	//Parse(...ParseHook) // Main parsing method with different parsing hooks
+	Close() // Gracefully finish work and stop watching
+}
+
+type BaseBrowser struct {
+	watcher   *fsnotify.Watcher
+	baseDir   string
+	bkFile    string
+	parseFunc func(*Browser)
+	bufferDB  *sql.DB
+	stats     *parserStats
+}
+
+type ChromeBrowser struct {
+}
