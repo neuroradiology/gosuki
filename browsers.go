397f05650bf9133ad7aa6b691fc58a866ec08647
Chakib Ben Ziane <contact@blob42.xyz>
2018-05-27 18:09:53 +0200

    update API

--- browsers.go
+++ browsers.go
@@ -3,7 +3,8 @@
 // All constants and common API for browsers should be implemented here.
 //
 // For *browser specific* implementation create a new file for that browser.
-// You must then implement a New<BrowserType>() IBrowser function. Then
+//
+// You must then implement a `New[BrowserType]()` IBrowser function and
 // implement parsing.
 package main
 
