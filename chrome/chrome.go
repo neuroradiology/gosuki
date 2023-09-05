1efedcb71db806e7cbc364eef6f2b465be2d24c8
blob42 <contact@blob42.xyz>
2023-09-05 22:59:24 +0200

    wip chrome module refactor

--- chrome/chrome.go
+++ chrome/chrome.go
@@ -1,1 +1,39 @@
 package chrome
+
+import (
+	"git.blob42.xyz/gomark/gosuki/modules"
+)
+
+// Chrome browser module
+type Chrome struct {
+
+	// holds the browsers.BrowserConfig
+	*ChromeConfig
+}
+
+// Returns a pointer to an initialized browser config
+func (c Chrome) Config() *modules.BrowserConfig {
+	return c.BrowserConfig
+}
+
+func (c Chrome) ModInfo() modules.ModInfo {
+	return modules.ModInfo{
+		ID: modules.ModID(c.Name),
+		New: func() modules.Module {
+			return NewChrome()
+		},
+	}
+}
+
+func NewChrome() *Chrome {
+	return &Chrome{
+		ChromeConfig: ChromeCfg,
+	}
+}
+
+func init(){
+  modules.RegisterBrowser(Chrome{ChromeConfig: ChromeCfg})
+}
+
+
+
--- chrome/config.go
+++ chrome/config.go
@@ -1,5 +1,42 @@
 package chrome
 
+import (
+	"git.blob42.xyz/gomark/gosuki/modules"
+	"git.blob42.xyz/gomark/gosuki/parsing"
+	"git.blob42.xyz/gomark/gosuki/tree"
+)
+
+
 const (
-	BookmarkFile = "Bookmarks"
+	BrowserName    = "chrome"
+	ChromeBaseDir  = "$HOME/.config/google-chrome"
+	DefaultProfile = "Default"
+)
+
+type ChromeConfig struct {
+	Profile                string
+	*modules.BrowserConfig `toml:"-"`
+	modules.ProfilePrefs `toml:"profile_options"`
+}
+
+var (
+	ChromeCfg = &ChromeConfig{
+		Profile:       DefaultProfile,
+		BrowserConfig: &modules.BrowserConfig{
+			Name:           BrowserName,
+			Type:           modules.TChrome,
+			BkDir:          "",
+			BkFile:         "Bookmarks",
+			//TODO: build watched paths
+			WatchedPaths:   []string{"/home/spike/.config/google-chrome-unstable/Default/Bookmarks"},
+			NodeTree: &tree.Node{
+				Name:   "ROOT",
+				Parent: nil,
+				Type: tree.RootNode,
+			},
+			Stats: &parsing.Stats{},
+			UseFileWatcher: true,
+		},
+		//TODO: profile
+	}
 )
