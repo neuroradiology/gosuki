6420594047e6655ee79b73f281549269e3e2be7a
blob42 <contact@blob42.xyz>
2023-09-06 14:26:50 +0200

    chrome module: impl Initializer and wip WatchRunner

--- chrome/config.go
+++ chrome/config.go
@@ -6,35 +6,33 @@ import (
 	"git.blob42.xyz/gomark/gosuki/tree"
 )
 
-
 const (
 	BrowserName    = "chrome"
 	ChromeBaseDir  = "$HOME/.config/google-chrome"
 	DefaultProfile = "Default"
+	RootNodeName   = "ROOT"
 )
 
 type ChromeConfig struct {
 	Profile                string
 	*modules.BrowserConfig `toml:"-"`
-	modules.ProfilePrefs `toml:"profile_options"`
+	modules.ProfilePrefs   `toml:"profile_options"`
 }
 
 var (
 	ChromeCfg = &ChromeConfig{
-		Profile:       DefaultProfile,
+		Profile: DefaultProfile,
 		BrowserConfig: &modules.BrowserConfig{
-			Name:           BrowserName,
-			Type:           modules.TChrome,
-			BkDir:          "",
-			BkFile:         "Bookmarks",
-			//TODO: build watched paths
-			WatchedPaths:   []string{"/home/spike/.config/google-chrome-unstable/Default/Bookmarks"},
+			Name:   BrowserName,
+			Type:   modules.TChrome,
+			BkDir:  "$HOME/.config/google-chrome/Default",
+			BkFile: "Bookmarks",
 			NodeTree: &tree.Node{
-				Name:   "ROOT",
+				Name:   RootNodeName,
 				Parent: nil,
-				Type: tree.RootNode,
+				Type:   tree.RootNode,
 			},
-			Stats: &parsing.Stats{},
+			Stats:          &parsing.Stats{},
 			UseFileWatcher: true,
 		},
 		//TODO: profile
