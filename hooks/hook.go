43321804bc53d2320855994c7a7fabb8d30c2f5e
blob42 <contact@blob42.xyz>
2023-09-16 17:25:25 +0200

    add licenses

--- hooks/hook.go
+++ hooks/hook.go
@@ -1,3 +1,24 @@
+//
+// Copyright ⓒ 2023 Chakib Ben Ziane <contact@blob42.xyz> and [`GoSuki` contributors]
+// (https://github.com/blob42/gosuki/graphs/contributors).
+//
+// All rights reserved.
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This file is part of GoSuki.
+//
+// GoSuki is free software: you can redistribute it and/or modify it under the terms of
+// the GNU Affero General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+//
+// GoSuki is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE.  See the GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with
+// gosuki.  If not, see <http://www.gnu.org/licenses/>.
+
 // Package hooks packages is the main feature of Gosuki. It permits to register
 // custom hooks that will be called during the parsing process of a bookmark
 // file. Hooks can be used to extract tags, commands or any custom data from a
